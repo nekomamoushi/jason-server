fc3fd91b140ce4efaed11ab324c81fb8d5c800f7
nekomamoushi <germione.grangere@gmail.com>
2019-08-19 12:05:54 +0200

    fix: invalid home page for Pypi

--- src/jason_server/__init__.py
+++ src/jason_server/__init__.py
@@ -3,7 +3,7 @@ __version__ = "0.2.0"
 
 __title__ = "jason-server"
 __description__ = "JSON as REST API"
-__url__ = "www.google.com"
+__url__ = "https://github.com/nekomamoushi/jason-server"
 
 __author__ = "Jerome Pradier"
 __author_email__ = "germione.grangere@gmail.com"
