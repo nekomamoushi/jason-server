16f786c91d51dedbc617f5b12d251a972470cf13
nekomamoushi <germione.grangere@gmail.com>
2019-08-21 20:14:30 +0200

    test: update for sorting feature

--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -6,16 +6,6 @@ import time
 
 def describe_endpoint():
 
-    home_url = "http://127.0.0.1:8100"
-    persons_url = "http://127.0.0.1:8100/persons"
-    persons_url_page1 = "http://127.0.0.1:8100/persons?_page=1"
-    persons_url_page2 = "http://127.0.0.1:8100/persons?_page=2"
-    persons_url_page1_limit3 = "http://127.0.0.1:8100/persons?_page=1&_limit=3"
-    persons_url_page2_limit3 = "http://127.0.0.1:8100/persons?_page=2&_limit=3"
-    persons_url_page3_limit3 = "http://127.0.0.1:8100/persons?_page=3&_limit=3"
-    persons_url_page4_limit3 = "http://127.0.0.1:8100/persons?_page=4&_limit=3"
-    persons_url_page5_limit3 = "http://127.0.0.1:8100/persons?_page=5&_limit=3"
-
     @pytest.fixture()
     def s():
         # Wait for jason_server
@@ -25,6 +15,9 @@ def describe_endpoint():
         yield s
 
     def get(s):
+        home_url = "http://127.0.0.1:8100"
+        persons_url = "http://127.0.0.1:8100/persons"
+
         r = s.get(home_url)
         assert 200 == r.status_code
 
@@ -35,6 +28,9 @@ def describe_endpoint():
         assert 13 == len(data['data'])
 
     def get_with_paginate(s):
+        persons_url_page1 = "http://127.0.0.1:8100/persons?_page=1"
+        persons_url_page2 = "http://127.0.0.1:8100/persons?_page=2"
+
         r = s.get(persons_url_page1)
         assert 200 == r.status_code
         data = r.json()
@@ -46,6 +42,12 @@ def describe_endpoint():
         assert 3 == len(data['data'])
 
     def get_with_paginate_and_limit(s):
+        persons_url_page1_limit3 = "http://127.0.0.1:8100/persons?_page=1&_limit=3"
+        persons_url_page2_limit3 = "http://127.0.0.1:8100/persons?_page=2&_limit=3"
+        persons_url_page3_limit3 = "http://127.0.0.1:8100/persons?_page=3&_limit=3"
+        persons_url_page4_limit3 = "http://127.0.0.1:8100/persons?_page=4&_limit=3"
+        persons_url_page5_limit3 = "http://127.0.0.1:8100/persons?_page=5&_limit=3"
+
         r = s.get(persons_url_page1_limit3)
         assert 200 == r.status_code
         data = r.json()
@@ -70,3 +72,19 @@ def describe_endpoint():
         assert 200 == r.status_code
         data = r.json()
         assert 1 == len(data['data'])
+
+    def get_with_sorting(s):
+        persons_url_sort_by_age = "http://127.0.0.1:8100/persons?_sort=age"
+        persons_url_sort_by_name = "http://127.0.0.1:8100/persons?_sort=name"
+
+        r = s.get(persons_url_sort_by_age)
+        assert 200 == r.status_code
+        data = r.json()
+        assert 21 ==data['data'][0]['age']
+        assert "Bell Hinton" == data['data'][0]['name']
+
+        r = s.get(persons_url_sort_by_name)
+        assert 200 == r.status_code
+        data = r.json()
+        assert 30 == data['data'][0]['age']
+        assert "Anastasia Coffey" == data['data'][0]['name']
