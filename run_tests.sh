2e31828ea5b6618667ee92bbd11230de6f75129c
nekomamoushi <germione.grangere@gmail.com>
2019-08-21 17:34:00 +0200

    fix: run all tests

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,9 @@
 # kill the current script and all the background processes.
 trap "kill 0" EXIT
 
-jason-server --port 8100 watch tests/data/big_database.json &
-pytest --disable-warnings -s tests/test_app.py
+# run unit tests
+pytest --disable-warnings tests/unit/
 
+# run integration tests
+jason-server --port 8100 watch tests/data/big_database.json &
+pytest --disable-warnings tests/integration/test_app.py
