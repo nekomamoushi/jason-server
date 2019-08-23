9962cb066925c0960f076d965dc21a745577847c
nekomamoushi <germione.grangere@gmail.com>
2019-08-23 18:25:24 +0200

    docs: update docstrings for database

--- src/jason_server/database.py
+++ src/jason_server/database.py
@@ -8,10 +8,26 @@ from jason_server.utils import open_database
 
 
 def tinydb_make_table(db, name):
+    """Create Tinydb Table for db
+
+    Args:
+        db(Tinydb): database object
+        name(str): table name to create
+
+    Returns:
+        Table: Table created
+    """
     return db.table(name)
 
 
 def tinydb_populate_table(table, data):
+    """Populate Tinydb Table
+
+    Args:
+        table(Table): Table object
+        data(dict): Resource to inject
+
+    """
     for elt in data:
         table.insert(elt)
 
@@ -27,6 +43,11 @@ class Database(object):
         self._endpoints = self._generate_tables()
 
     def _generate_tables(self):
+        """Generate tables from database
+
+        Returns:
+            list: table names
+        """
         resources = [name for name in self._json]
         for name in resources:
             table = tinydb_make_table(self._db, name)
@@ -35,14 +56,34 @@ class Database(object):
 
     @property
     def json(self):
+        """Return Dict Repr for the database
+
+        Returns:
+            dict: database as dict
+        """
         return self._json
 
     @property
     def endpoints(self):
+        """Return a list of the table's name
+
+        Returns:
+            list: table name
+        """
         return self._endpoints
 
     def resources(self):
+        """Return a list of the table's name
+
+        Returns:
+            list: table name
+        """
         return [table for table in self._db.tables() if table != "_default"]
 
     def resource(self, name):
+        """Return the table resource
+
+        Returns:
+            Table: Table resource
+        """
         return self._db.table(name)
